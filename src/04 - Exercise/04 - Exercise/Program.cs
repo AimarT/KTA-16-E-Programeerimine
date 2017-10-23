ce7e9dba30dc18edcd0ee910d80201f31f608768
AimarT <Aimar.Tamm@tptlive.ee>
2017-10-23 14:49:04 +0300

    04 - Ex
    kahe arvu liitmine

---
+++ src/04 - Exercise/04 - Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina olen kalkulaatori programm. Ütle mulle 2 numbrit ja ma ütlen Sulle nende summa");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int arv = int.Parse(input1);
+         
+       
+            Console.WriteLine("Esimene number:" + input1);
+            Console.WriteLine("Teine number:" + input2);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Tulemuseks on:" + Convert);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
+
+        }
+    }
+}
