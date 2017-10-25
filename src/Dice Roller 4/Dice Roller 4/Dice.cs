6809af11fc84d29a1e9900b8f3221e24e3502fb4
AimarT <Aimar.Tamm@tptlive.ee>
2017-10-25 13:55:21 +0300

    25.10

---
+++ src/Dice Roller 4/Dice Roller 4/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Roller_4
+{
+    class Dice
+    {
+        public int Roll ()
+        {
+            var random = new Random();
+            var roll = 0;
+
+            return roll;
+        }
+    }
+}
