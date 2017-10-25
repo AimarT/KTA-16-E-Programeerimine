081d3ba521280ea03c421536e69bee33d47c3321
AimarT <Aimar.Tamm@tptlive.ee>
2017-10-25 15:58:36 +0300

    25.10

--- src/Dice Roller 4/Dice Roller 4/Program.cs
+++ src/Dice Roller 4/Dice Roller 4/Program.cs
@@ -14,19 +14,26 @@ namespace Dice_Roller
 
             Console.WriteLine("> /roll 3d6 2d8");
             Console.WriteLine(" ");
+            
+            DiceRoller diceRoller = new DiceRoller();
+            List<DiceRoll> diceRolls = diceRoller.Roll(
+                new List<Dice_Roller_4.Dice> { Dice_Roller_4.Dice.D6, Dice_Roller_4.Dice.D6, Dice_Roller_4.Dice.D8, Dice_Roller_4.Dice.D8 });
 
          
 
             var random = new Random();
             var total = 0;
-
+            var d6 = new Dice_Roller_4.(6, "d6");
             for (var i = 0; i < 3; i++)
             {
-                var roll = random.Next(1, 7);
+                var roll = d6.Roll(1, 7);
                 total += roll;
 
-                Console.WriteLine($"1d6 {roll}");
-            }for (var i =0; i<2; i++)
+                Console.WriteLine($"1{d6.Description}:{roll}");
+            }
+
+            var d8 = DiceRoller.D8
+            for (var i =0; i<2; i++)
 
 
 
